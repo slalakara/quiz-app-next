8fe46b48834755b31af72c7ad20a16369da330fa
Sıla <silakara1453@gmail.com>
2024-10-01 13:02:30 +0300

    chooseCategory.jsx te kategorilere link eklendi. her kategori için ayrı dosyalar açıldı.

--- components/chooseCategory.jsx
+++ components/chooseCategory.jsx
@@ -1,4 +1,5 @@
 import konular from "@/app/data/konular.json"
+import Link from "next/link";
 import data from "@/app/data/data.json"
 import Image from "next/image";
 import HTML from "@/app/img/HTML.png"
@@ -12,17 +13,17 @@ export default function QuizSelector() {
     <div className="quiz-selector">
       <div className="Startcontainer">
         <div className="StartHeader">
-          <h4>Welcome to the <br /><h1>Frontend Quiz!</h1></h4>
+          <h4>Welcome to the <br /><h5>Frontend Quiz!</h5></h4>
           <p>Pick a subject to get started.</p>
         </div>
         <div className="categories">
           {konular.map(x => (
             <button className="categoryItem" key={x.id}>
               <span className="category-icon">
-                {x.konu === 'HTML' && <Image style={{backgroundColor: '#FFF1E9'}} src={HTML} width={40} height={40} alt="HTML Icon" />}
-                {x.konu === 'CSS' && <Image style={{backgroundColor: '#E0FDEF'}} src={CSS} width={40} height={40} alt="CSS Icon" />}
-                {x.konu === 'JS' && <Image style={{backgroundColor: '#EBF0FF'}} src={JS} width={40} height={40} alt="JS Icon" />}
-                {x.konu === 'Accessibility' && <Image style={{backgroundColor: '#F6E7FF'}} width={40} height={40} src={Acs} alt="Accessibility Icon" />}
+                <Link href="/HTML">{x.konu === 'HTML' && <Image style={{backgroundColor: '#FFF1E9'}} src={HTML} width={40} height={40} alt="HTML Icon" />}</Link>
+                <Link href="/CSS">{x.konu === 'CSS' && <Image style={{backgroundColor: '#E0FDEF'}} src={CSS} width={40} height={40} alt="CSS Icon" />}</Link>
+                <Link href="/JS">{x.konu === 'JS' && <Image style={{backgroundColor: '#EBF0FF'}} src={JS} width={40} height={40} alt="JS Icon" />}</Link>
+                <Link href="/Accessibility">{x.konu === 'Accessibility' && <Image style={{backgroundColor: '#F6E7FF'}} width={40} height={40} src={Acs} alt="Accessibility Icon" />}</Link>
               </span>
               <h3>{x.konu}</h3>
             </button>
